b9bb642c8b32074cc787d7c7ac477c5f27c6413e
daniyaalmalik <daniyal.malik@sybrid.com>
2023-11-27 13:58:17 +0500

    http://124.29.235.10:81/SybridGitServer/RevX_MobileApp.git

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>ReactNative App</Text>
+    </View>
+  );
+}
+
+export default App;
