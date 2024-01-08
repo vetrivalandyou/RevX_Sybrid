4f640c48c9490f653ef54c3aa11326c4776da8c2
“abidabegum” <abdia.begum@sybrid.com>
2024-01-08 07:51:49 -0800

    working in graph

--- source/screens/Location/index.js
+++ source/screens/Location/index.js
@@ -1,5 +1,5 @@
-import React, {useRef} from 'react';
-import {ImageBackground, Text, View, Image, Button} from 'react-native';
+import React, { useRef } from 'react';
+import { ImageBackground, Text, View, Image, Button } from 'react-native';
 import Search from '../../components/atom/Search/Search';
 import Header from '../../components/molecules/Header';
 import CustomIcon, {
@@ -7,28 +7,34 @@ import CustomIcon, {
 } from '../../components/molecules/CustomIcon/CustomIcon';
 import Screen from '../../components/atom/ScreenContainer/Screen';
 import appColors from '../../AppConstants/appColors';
-import {AppImages} from '../../AppConstants/AppImages';
+import { AppImages } from '../../AppConstants/AppImages';
 import AuthHeader from '../../components/molecules/AuthHeader';
 import RBSheet from 'react-native-raw-bottom-sheet';
-import {screenSize} from '../../components/atom/ScreenSize';
+import { screenSize } from '../../components/atom/ScreenSize';
 import BottomSheet from '../../components/molecules/BottomSheetContent/BottomSheet';
 import LocationBottom from '../LocationBottom';
 import ReferFriendsSheet from '../ReferFriendsSheet';
 import LogoutBottom from '../LogoutBottom';
+import constants from "../../AppConstants/Constants.json"
+import { useNavigation } from '@react-navigation/native';
 
-const LocationScreen = ({navigation}) => {
+const LocationScreen = () => {
+
+  const navigation = useNavigation();
+
   const refRBSheet = useRef();
 
   return (
     <Screen
       statusBarColor={appColors.Black}
       barStyle="light-content"
-      viewStyle={{backgroundColor: appColors.Black, padding: 15, flex: 0.9}}>
+      viewStyle={{ backgroundColor: appColors.Black, padding: 15, flex: 0.9 }}>
       <BottomSheet ref={refRBSheet} Height={screenSize.height - 452}>
         <LocationBottom refRBSheet={refRBSheet} />
       </BottomSheet>
 
-      <View style={{flex: 0.1}}>
+      <View style={{ flex: 0.1 }}>
+
         <Header
           lefttIcoType={Icons.Ionicons}
           onPressLeftIcon={() => navigation.goBack()}
@@ -37,6 +43,8 @@ const LocationScreen = ({navigation}) => {
           rightIcoName={'bell'}
           rightIcoType={Icons.SimpleLineIcons}
           logIn={'success'}
+
+
           rightIcoSize={20}
           leftIcoStyle={{
             backgroundColor: appColors.lightBlack,
@@ -46,19 +54,20 @@ const LocationScreen = ({navigation}) => {
             justifyContent: 'center',
             alignItems: 'center',
           }}
-          headerTextViewStyle={{alignItems: 'center'}}
+          headerTextViewStyle={{ alignItems: 'center' }}
+          onPressRightIcon={() => navigation.navigate(constants.screen.Notification)}
         />
       </View>
 
-      <View style={{flex: 0.1}}>
+      <View style={{ flex: 0.1 }}>
         <Search
           leaftIconType={Icons.Ionicons}
           leftIconName={'filter'}
-          style={{marginVertical: 11}}
+          style={{ marginVertical: 11 }}
         />
       </View>
 
-      <View style={{flex: 0.8, borderRadius: 30}}>
+      <View style={{ flex: 0.8, borderRadius: 30 }}>
         <ImageBackground
           style={{
             flex: 1,
@@ -74,7 +83,7 @@ const LocationScreen = ({navigation}) => {
             size={40}
             color={appColors.Goldcolor}
             onPress={() => refRBSheet.current.open()}
-            style={{alignSelf: 'center'}}
+            style={{ alignSelf: 'center' }}
           />
         </ImageBackground>
       </View>
