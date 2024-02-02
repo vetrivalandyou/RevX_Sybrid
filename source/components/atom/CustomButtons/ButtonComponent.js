123d5a759f82755d1cd238ac740efe097e9736ed
m.anas <m.anas@sybrid.com>
2024-02-02 15:26:59 +0500

    DoneWithDesignAndFocusedTextBoxofOTP

--- source/components/atom/CustomButtons/ButtonComponent.js
+++ source/components/atom/CustomButtons/ButtonComponent.js
@@ -4,21 +4,25 @@ import {ActivityIndicator, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import appColors from '../../../AppConstants/appColors';
 
-const ButtonComponent = ({onPress, title, style, btnTextColor, btnColor, isLoading}) => {
+const ButtonComponent = ({
+  onPress,
+  title,
+  style,
+  btnTextColor,
+  btnColor,
+  isLoading,
+  disable,
+}) => {
   return (
     <TouchableOpacity
-      style={[styles.container, style, btnColor]}
-      onPress={onPress}>
-        {
-          isLoading == true ? 
-          (
-            <ActivityIndicator size='small' color={appColors.White} />
-          ) : 
-          (
-            <Text style={[styles.btnText, btnTextColor]}>{title}</Text>
-          )
-        }
-     
+      style={[styles.container, style, btnColor, {backgroundColor: btnColor ? btnColor : appColors.Goldcolor,}]}
+      onPress={onPress}
+      disabled={disable}>
+      {isLoading == true ? (
+        <ActivityIndicator size="small" color={appColors.White} />
+      ) : (
+        <Text style={[styles.btnText, btnTextColor]}>{title}</Text>
+      )}
     </TouchableOpacity>
   );
 };
--- source/screens/Auth/ForgotPassword/OTP_Verification.js
+++ source/screens/Auth/ForgotPassword/OTP_Verification.js
@@ -1,23 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {Text, TextInput, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import appColors from '../../../AppConstants/appColors';
 import Screen from '../../../components/atom/ScreenContainer/Screen';
 import AuthHeader from '../../../components/molecules/AuthHeader';
-import SimpleTextField from '../../../components/molecules/TextFeilds/SimpleTextField';
 import constants from '../../../AppConstants/Constants.json';
 
 import ButtonComponent from '../../../components/atom/CustomButtons/ButtonComponent';
 
 const OTP_Verification = ({navigation}) => {
-  const [verificationCode, setVerificationCode] = useState([
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  ]);
   const [timer, setTimer] = useState(60);
+
+  useEffect(() => {
+    otpInputRefs[0].current.focus();
+  }, []);
 
   useEffect(() => {
     let interval;
@@ -37,13 +32,19 @@ const OTP_Verification = ({navigation}) => {
       .padStart(2, '0')}`;
   };
 
-  const handleVerificationCodeChange = (text, index) => {
-    const numericRegex = /^[0-9]*$/;
-    if (numericRegex.test(text) && text.length <= 1) {
-      const newVerificationCode = [...verificationCode];
-      newVerificationCode[index] = text;
-      setVerificationCode(newVerificationCode);
+  const otpInputRefs = Array.from({length: 6}, () => useRef(0));
+  const [focusedIndex, setFocusedIndex] = useState(0);
+
+  const handleOTPInputChange = (index, value) => {
+    const otpValues = Array.from(value);
+    otpValues[index] = value.charAt(value.length - 1);
+    if (index < otpInputRefs.length - 1 && value !== '') {
+      otpInputRefs[index + 1].current.focus();
     }
+  };
+
+  const handleInputFocus = index => {
+    setFocusedIndex(index);
   };
 
   return (
@@ -58,101 +59,60 @@ const OTP_Verification = ({navigation}) => {
           onPress={() => navigation.goBack()}
         />
       </View>
-      <View
-        style={{
-          backgroundColor: 'white',
-          flex: 0.8,
-          padding: 15,
-          backgroundColor: appColors.Black,
-        }}>
+      <View style={OTP_Verification_Style.subContainer}>
         <View style={{flex: 0.1}}>
-          <Text
-            style={{
-              color: 'white',
-              fontSize: 26,
-              textAlign: 'left',
-              marginTop: 20,
-            }}>
+          <Text style={OTP_Verification_Style.headingStyle}>
             Verification Code
           </Text>
         </View>
         <View style={{flex: 0.1}}>
-          <Text
-            style={{
-              color: 'white',
-              fontSize: 15,
-              textAlign: 'left',
-              marginTop: 12,
-              lineHeight: 22,
-            }}>
+          <Text style={OTP_Verification_Style.headingDescription}>
             Enter 6 digit verification code sent to your mobile number
           </Text>
         </View>
-        <View
-          style={{
-            flex: 0.1,
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            marginTop: 50,
-          }}>
-          {verificationCode.map((digit, index) => (
+        <View style={OTP_Verification_Style.textBoxView}>
+          {Array.from({length: 6}).map((digit, index) => (
             <TextInput
               key={index}
-              style={{
-                borderLeftWidth: 1,
-                borderBottomWidth: 1,
-                borderRightWidth: 1,
-                borderTopWidth: 1,
-                borderBottomColor: 'white',
-                color: 'white',
-                fontSize: 22,
-                width: 50,
-                borderRadius: 12,
-                textAlign: 'center',
-                borderColor: 'white',
-              }}
-              placeholder={'0'}
+              style={[
+                OTP_Verification_Style.textInputStyle,
+                {
+                  borderColor:
+                    focusedIndex === index
+                      ? appColors.White
+                      : appColors.GrayColor,
+                },
+              ]}
+              placeholder={''}
               placeholderTextColor={appColors.LightGray}
               keyboardType="numeric"
               maxLength={1}
-              value={digit}
-              onChangeText={text => handleVerificationCodeChange(text, index)}
+              onFocus={() => handleInputFocus(index)}
+              onChangeText={text => handleOTPInputChange(index, text)}
+              ref={otpInputRefs[index]}
             />
           ))}
         </View>
-        <View
-          style={{
-            flex: 0.1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 12,
-          }}>
+        <View style={OTP_Verification_Style.resendTimerView}>
           <Text style={{color: 'white', fontSize: 16}}>
-            Resend Code in {formatTime(timer)} minutes
+            Resend Code in {formatTime(timer)} seconds
           </Text>
         </View>
-        <View style={{flex: 0.1}}>
-          <Text
-            style={{
-              color: 'white',
-              fontSize: 15,
-              textAlign: 'center',
-              marginTop: 12,
-              lineHeight: 22,
-            }}>
+        <View style={OTP_Verification_Style.dontReceiveCodeView}>
+          <Text style={OTP_Verification_Style.dontReceiveCodeText}>
             If you don't recieve OTP with in 60 seconds please resend again.
           </Text>
         </View>
 
-        <View
-          style={{
-            flex: 0.1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginTop: 12,
-          }}>
+        <View style={OTP_Verification_Style.buttonView}>
           <ButtonComponent
             title={'Resend OTP'}
+            disable={formatTime(timer) == '00:00' ? false : true}
+            btnColor={
+              formatTime(timer) == '00:00'
+                ? appColors.Goldcolor
+                : appColors.disableGrayColor
+            }
             onPress={() =>
               navigation.navigate(constants.AuthScreen.ForgotCheckEmail)
             }
@@ -162,5 +122,65 @@ const OTP_Verification = ({navigation}) => {
     </Screen>
   );
 };
+
+const OTP_Verification_Style = StyleSheet.create({
+  subContainer: {
+    backgroundColor: 'white',
+    flex: 0.8,
+    padding: 15,
+    backgroundColor: appColors.Black,
+  },
+  headingStyle: {
+    color: 'white',
+    fontSize: 26,
+    textAlign: 'left',
+    marginTop: 20,
+  },
+  headingDescription: {
+    color: 'white',
+    fontSize: 15,
+    textAlign: 'left',
+    marginTop: 12,
+    lineHeight: 22,
+  },
+  textBoxView: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 50,
+  },
+  textInputStyle: {
+    borderLeftWidth: 1,
+    borderBottomWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderBottomColor: appColors.White,
+    color: appColors.White,
+    fontSize: 22,
+    width: 50,
+    borderRadius: 12,
+    textAlign: 'center',
+  },
+  resendTimerView: {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 12,
+  },
+  dontReceiveCodeView: {flex: 0.1, justifyContent: 'center'},
+  dontReceiveCodeText: {
+    color: appColors.White,
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 12,
+    lineHeight: 22,
+  },
+  buttonView: {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 12,
+  },
+});
 
 export default OTP_Verification;
