5f8bc6a8c50daca28677cf995f7efe4cfbcf5c48
prenaparpiani <prena43631@sybrid.com>
2024-02-12 00:13:18 -0800

    ProjectSetup

---
+++ source/screens/BarberProfie/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+const BarberShop =()=>{
+    return(
+        <View>
+<Text>BarberShop</Text>
+        </View>
+    )
+
+}
+export default BarberShop;
