88326575630783e6427ed2137362fd18f9f86fa0
prenaparpiani <prena43631@sybrid.com>
2024-02-14 00:19:26 -0800

    SettingHeader

--- source/components/molecules/Header/index.js
+++ source/components/molecules/Header/index.js
@@ -31,7 +31,6 @@ const Header = ({
   const navigation = useNavigation();
 
   return (
-<<<<<<< HEAD
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.headerView}>
         <View style={[styles.header, headerSubView]}>
@@ -88,13 +87,13 @@ const Header = ({
                 }
 
                 <View style={iconContainerStyle1}>
-                  <View style={{iconStyle}}>
-                  <CustomIcon
-                    type={rightIcoType}
-                    name={rightIcoName}
-                    size={rightIcoSize}
-                    color={appColors.White}
-                  />
+                  <View style={{ iconStyle }}>
+                    <CustomIcon
+                      type={rightIcoType}
+                      name={rightIcoName}
+                      size={rightIcoSize}
+                      color={appColors.White}
+                    />
                   </View>
                 </View>
 
@@ -102,53 +101,6 @@ const Header = ({
               </View>
             </TouchableOpacity>
           </View>
-
-=======
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.headerView}>
-        <View style={[styles.header, headerSubView]}>
-          <View style={{flex: 0.15, justifyContent: 'center'}}>
-            {image ? (
-              <Image
-                source={AppImages.SuperAdmin}
-                style={{width: 50, height: 50}}
-              />
-            ) : (
-              <TouchableOpacity onPress={onPressLeftIcon}>
-                <CustomIcon
-                  type={lefttIcoType}
-                  name={leftIcoName}
-                  size={leftIcoSize}
-                  color={appColors.White}
-                />
-              </TouchableOpacity>
-            )}
-          </View>
-          <View style={[styles.headerTextView, headerTextViewStyle]}>
-            <Text style={[styles.headerText, headerTextt]}>{headerText}</Text>
-          </View>
-
-          <View style={{flex: 0.15, justifyContent: 'center'}}>
-            <TouchableOpacity
-              onPress={
-                onPressRightIcon
-                  ? onPressRightIcon
-                  : () => {
-                      navigation.navigate(
-                        constants.AdminScreens.AdminNotification,
-                      );
-                    }
-              }
-              style={leftIcoStyle}>
-              <CustomIcon
-                type={rightIcoType}
-                name={rightIcoName}
-                size={rightIcoSize}
-                color={appColors.White}
-              />
-            </TouchableOpacity>
-          </View>
->>>>>>> 18dda5058208440f8e87d828f39525a361f3b417
         </View>
       </View>
     </SafeAreaView>
