5da27068fcc851986306141abc8b429386df6296
MuhammadAnas41 <muhdanassaleem@gmail.com>
2024-03-26 15:07:06 +0500

    ManageVansCrudCompleted

--- source/screens/AdminBoard/ManageAssignments/Assignment.js
+++ source/screens/AdminBoard/ManageAssignments/Assignment.js
@@ -1,94 +1,102 @@
-import { ActivityIndicator, Image, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
-import { screenSize } from '../../../components/atom/ScreenSize';
+import {
+  ActivityIndicator,
+  Image,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {screenSize} from '../../../components/atom/ScreenSize';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Entypo from 'react-native-vector-icons/Entypo';
 import ButtonComponent from '../../../components/atom/CustomButtons/ButtonComponent';
 import styles from './styles';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 // import BottomSheet from '../../components/atom/BottomSheet';
 
 import Screen from '../../../components/atom/ScreenContainer/Screen';
 import constants from '../../../AppConstants/Constants.json';
 import Header from '../../../components/molecules/Header';
-import { Icons } from '../../../components/molecules/CustomIcon/CustomIcon';
+import {Icons} from '../../../components/molecules/CustomIcon/CustomIcon';
 
 import BottomSheet from '../../../components/molecules/BottomSheetContent/BottomSheet';
 import appColors from '../../../AppConstants/appColors';
 import DeleteAssignment from './DeleteAssignment';
-import { endPoint } from '../../../AppConstants/urlConstants';
-import { PostRequest } from '../../../services/apiCall';
-import { AppImages } from '../../../AppConstants/AppImages';
-import { SimpleSnackBar } from '../../../components/atom/Snakbar/Snakbar';
+import {endPoint} from '../../../AppConstants/urlConstants';
+import {PostRequest} from '../../../services/apiCall';
+import {AppImages} from '../../../AppConstants/AppImages';
+import {SimpleSnackBar} from '../../../components/atom/Snakbar/Snakbar';
 
-const Assignments = ({ }) => {
+const Assignments = ({}) => {
   const navigation = useNavigation();
   const [selectedItem, setSelectedItem] = useState(null);
   const [vanAssignment, setvanAssignment] = useState();
-  const [loader,setLoader] = useState(true)
-  
-  useEffect(()=>{
-PostbarberVanAssignment();
-  })
-const PostbarberVanAssignment =()=>{
-  payload = {
-    
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    PostbarberVanAssignment();
+  },[]);
+
+  const PostbarberVanAssignment = () => {
+    payload = {
       id: null,
       barberId: null,
       vanId: null,
       operations: 0,
       createdBy: 1,
-      userIP:  '::1',
-    
-  }
+      userIP: '::1',
+    };
 
-  PostRequest(endPoint.GET_BARBER_VANASSIGNMENT,payload)
-  .then(res => {
-    if (res?.data?.code == 200) {
-      setvanAssignment(res?.data?.data)
-      setLoader(false)
-     
-    } else {
-      SimpleSnackBar(res?.data?.message);
-      console.log('................else',res.data)
-setLoader(false)
-    }
-  })
-  .catch(err => {
-    SimpleSnackBar(messages.Catch, appColors.Red);
-    setLoader(false)
-  });
-};
+    PostRequest(endPoint.GET_BARBER_VANASSIGNMENT, payload)
+      .then(res => {
+        if (res?.data?.code == 200) {
+          setvanAssignment(res?.data?.data);
+          setLoader(false);
+        } else {
+          SimpleSnackBar(res?.data?.message);
+          console.log('................else', res.data);
+          setLoader(false);
+        }
+      })
+      .catch(err => {
+        SimpleSnackBar(messages.Catch, appColors.Red);
+        setLoader(false);
+      });
+  };
 
   return (
-    <Screen viewStyle={{ flex: 1, padding: 15, backgroundColor: appColors.Black }} statusBarColor={appColors.Black} >
-      <View style={{ flex: 0.1, backgroundColor: appColors.Black }}>
+    <Screen
+      viewStyle={{flex: 1, padding: 15, backgroundColor: appColors.Black}}
+      statusBarColor={appColors.Black}>
+      <View style={{flex: 0.1, backgroundColor: appColors.Black}}>
         <Header
-          headerSubView={{ marginHorizontal: 5 }}
+          headerSubView={{marginHorizontal: 5}}
           lefttIcoType={Icons.Ionicons}
           onPressLeftIcon={() => navigation.goBack()}
           leftIcoName={'chevron-back'}
           headerText={'Assignments'}
         />
       </View>
-      {loader?( <ActivityIndicator
-            size="large"
-            color="#C79646"
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
-          />):(
-            <ScrollView style={{ flex: 0.8 }}>
-       { vanAssignment?.map(item => (
-          <Servicelist
-            key={item.vanId}
-            item={item}
-            selected={selectedItem === item.vanId}
-            onPress={() => setSelectedItem(item.vanId)}
-
-          />
-        ))}
+      {loader ? (
+        <ActivityIndicator
+          size="large"
+          color="#C79646"
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+        />
+      ) : (
+        <ScrollView style={{flex: 0.8}}>
+          {vanAssignment?.map(item => (
+            <Servicelist
+              key={item.vanId}
+              item={item}
+              selected={selectedItem === item.vanId}
+              onPress={() => setSelectedItem(item.vanId)}
+            />
+          ))}
         </ScrollView>
       )}
-    
 
       <View style={styles.buttonView}>
         <ButtonComponent
@@ -98,10 +106,12 @@ setLoader(false)
             bottom: 1,
             position: 'absolute',
           }}
-          btnTextColor={{ color: 'white' }}
+          btnTextColor={{color: 'white'}}
           title={'Add Assignment'}
           onPress={() =>
-            navigation.navigate(constants.AdminScreens.EditAssignment, { isAdded: true })
+            navigation.navigate(constants.AdminScreens.EditAssignment, {
+              isAdded: true,
+            })
           }
         />
       </View>
@@ -109,16 +119,13 @@ setLoader(false)
   );
 };
 
-
-
-const Servicelist = ({ item, onPress, selected, }) => {
-
+const Servicelist = ({item, onPress, selected}) => {
   const refRBSheet = useRef();
   const navigation = useNavigation();
   const handleEditPress = () => {
     navigation.navigate(constants.AdminScreens.EditAssignment, {
       serviceName: item.vanName,
-      isAdded: false
+      isAdded: false,
     });
   };
 
@@ -127,7 +134,7 @@ const Servicelist = ({ item, onPress, selected, }) => {
       <View
         style={[
           styles.container,
-          selected && { borderColor: '#c79647', borderWidth: 1.25 },
+          selected && {borderColor: '#c79647', borderWidth: 1.25},
         ]}>
         <View style={styles.Subcontainer}>
           <View style={styles.textView}>
@@ -137,12 +144,15 @@ const Servicelist = ({ item, onPress, selected, }) => {
           <TouchableOpacity
             onPress={handleEditPress}
             style={styles.editImageView}>
-            <Image source={AppImages.Editimage}style={styles.editImageStyle} />
+            <Image source={AppImages.Editimage} style={styles.editImageStyle} />
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => refRBSheet.current.open()}
             style={styles.DeleteimageView}>
-            <Image source={AppImages.deleteimage} style={styles.Deleteimagestyle} />
+            <Image
+              source={AppImages.deleteimage}
+              style={styles.Deleteimagestyle}
+            />
           </TouchableOpacity>
 
           <BottomSheet ref={refRBSheet} Height={200}>
--- source/screens/AdminBoard/ManageVans/styles.js
+++ source/screens/AdminBoard/ManageVans/styles.js
@@ -1,5 +1,6 @@
-import { StyleSheet } from 'react-native';
-import { screenSize } from '../../../components/atom/ScreenSize';
+import {StyleSheet} from 'react-native';
+import {screenSize} from '../../../components/atom/ScreenSize';
+import appColors from '../../../AppConstants/appColors';
 
 export default StyleSheet.create({
   container: {
@@ -10,99 +11,119 @@ export default StyleSheet.create({
     backgroundColor: '#252525',
     marginVertical: 5,
     paddingHorizontal: 5,
-
     justifyContent: 'center',
-
   },
   buttonView: {
     flex: 0.1,
     justifyContent: 'center',
     alignItems: 'center',
-
   },
   Subcontainer: {
     flexDirection: 'row',
     flex: 1,
   },
   textView: {
-    flex: 0.7,
+    flex: 0.5,
     justifyContent: 'center',
     paddingLeft: 20,
+    flexWrap: 'wrap',
   },
 
   textStyle: {
     color: 'white',
-    fontSize: 15,
+    fontSize: 14,
   },
   editImageView: {
-    flex: 0.15,
+    flex: 0.2,
     justifyContent: 'center',
     alignItems: 'flex-end',
   },
   editImageStyle: {
-    width: '40%',
-    height: '40%',
+    width: '35%',
+    height: '35%',
   },
 
   DeleteimageView: {
-    flex: 0.15,
+    flex: 0.2,
     justifyContent: 'center',
     alignItems: 'center',
   },
 
   Deleteimagestyle: {
-    width: '40%',
-    height: '40%',
+    width: '35%',
+    height: '35%',
   },
   updateprofileView: {
-    flex: 1, justifyContent: 'center', alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  buttonouterView: { flexDirection: 'row', marginHorizontal: 10, },
-  camerabuttonview: { flex: 0.5, justifyContent: 'center', alignItems: 'center', },
-  gallerybuttonview: { flex: 0.5, justifyContent: 'center', alignItems: 'center' },
-  ProfileMainView:{flex:0.35,},
-  ProfileouterView:{ flex: 1, justifyContent:"center", alignItems:"center",},
-  profileView:{ width:"34%",justifyContent:"center", alignItems:"center",height:'58%',backgroundColor:'black'},
-  imageStyle:{  width: '100%', height: '100%', borderRadius: 80, borderWidth: 3, borderColor: appColors.Goldcolor  },
-  Iconstyle:{ position: 'absolute', left: screenSize.width /4, top: screenSize.height / 8.3,},
-textFieldView:{ flex: 0.19, justifyContent: 'space-evenly', },
-buttonStyle:{
-  backgroundColor: '#C79646',
-  paddingVertical: Platform.OS == 'ios' ? 17 : 13,
-  bottom: 1,
-  position: 'absolute',
-},
-validationTextview:
-{
-  marginLeft: 12,
-  justifyContent: 'center',
-},
-validationTextStyle:{ color: appColors.Goldcolor, fontSize: 12 },
+  buttonouterView: {flexDirection: 'row', marginHorizontal: 10},
+  camerabuttonview: {flex: 0.5, justifyContent: 'center', alignItems: 'center'},
+  gallerybuttonview: {
+    flex: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ProfileMainView: {flex: 0.35},
+  ProfileouterView: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  profileView: {
+    width: '34%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '58%',
+    backgroundColor: 'black',
+  },
+  imageStyle: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 80,
+    borderWidth: 3,
+    borderColor: appColors.Goldcolor,
+  },
+  Iconstyle: {
+    position: 'absolute',
+    left: screenSize.width / 4,
+    top: screenSize.height / 8.3,
+  },
+  textFieldView: {flex: 0.19, justifyContent: 'space-evenly'},
+  buttonStyle: {
+    backgroundColor: '#C79646',
+    paddingVertical: Platform.OS == 'ios' ? 17 : 13,
+    bottom: 1,
+    position: 'absolute',
+  },
+  validationTextview: {
+    marginLeft: 12,
+    justifyContent: 'center',
+  },
+  validationTextStyle: {color: appColors.Goldcolor, fontSize: 12},
 
-mainView:{flex: 1, marginVertical: 15,},
-DeletetitleView:{flex: 0.4, alignItems: 'center', justifyContent: 'center',},
-titleTextStyle:{color: '#E81F1C', fontSize: 18},
-TextView:{flex: 0.6, paddingHorizontal: '20%'},
-TextStyle:{color: 'white', fontSize: 16, textAlign: 'center'},
-buttonsMainView:{
-  flex: 0.4,
-  justifyContent: 'center',
-  flexDirection: 'row',
-  justifyContent: 'space-evenly',
-},
-CanclebuttonView:{flex: 0.4, justifyContent: 'center', alignItems: 'flex-end'},
-CanclebuttonStyle:{
-backgroundColor: '#424242',
-paddingVertical: 13,
-width: '90%',
-},
-DeleteButtonView:{flex: 0.6, justifyContent: 'center', alignItems: 'center'},
-DeleteButtonStyle:{
-backgroundColor: '#E81F1C',
-paddingVertical: 13,
-width: '85%',
-},
-
-
+  mainView: {flex: 1, marginVertical: 15},
+  DeletetitleView: {flex: 0.4, alignItems: 'center', justifyContent: 'center'},
+  titleTextStyle: {color: '#E81F1C', fontSize: 18},
+  TextView: {flex: 0.6, paddingHorizontal: '20%'},
+  TextStyle: {color: 'white', fontSize: 16, textAlign: 'center'},
+  buttonsMainView: {
+    flex: 0.4,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  CanclebuttonView: {
+    flex: 0.4,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  CanclebuttonStyle: {
+    backgroundColor: '#424242',
+    paddingVertical: 13,
+    width: '90%',
+  },
+  DeleteButtonView: {flex: 0.6, justifyContent: 'center', alignItems: 'center'},
+  DeleteButtonStyle: {
+    backgroundColor: appColors.Red,
+    paddingVertical: 13,
+    width: '85%',
+  },
 });
-
