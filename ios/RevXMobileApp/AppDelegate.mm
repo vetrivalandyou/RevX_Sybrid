1a0c8e41fa736f1eca0238d2906ba03e94daac75
m.anas <m.anas@sybrid.com>
2024-04-02 16:17:48 +0500

    Revert "IOS_Google Maps Configured"
    This reverts commit 8f1b3e5cfc739b334bb145da1eff3892a18a9b46.

--- ios/RevXMobileApp/AppDelegate.mm
+++ ios/RevXMobileApp/AppDelegate.mm
@@ -2,17 +2,11 @@
 
 #import <React/RCTBundleURLProvider.h>
 
-#import <GoogleMaps/GoogleMaps.h>
-//Now paste this line (it should be the first call of the method 
-//so paste it the first curly braces at the top)
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"RevXMobileApp";
-
-  [GMSServices provideAPIKey:@"AIzaSyC7Y3a-Q8qZXj5XgLzpHa92b_nw3sR8aWE"];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
