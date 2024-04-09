79fa6d0c3e9145da6b2054c72d5c9e770ae78baa
m.anas <m.anas@sybrid.com>
2024-04-09 16:45:57 +0500

    Google Maps Configured on IOS

--- ios/RevXMobileApp/AppDelegate.mm
+++ ios/RevXMobileApp/AppDelegate.mm
@@ -2,10 +2,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyC7Y3a-Q8qZXj5XgLzpHa92b_nw3sR8aWE"];
   self.moduleName = @"RevXMobileApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
