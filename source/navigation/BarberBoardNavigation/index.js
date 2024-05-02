c4140a824be112b6292e780760b75d22a91b8fda
MuhammadAnas41 <muhdanassaleem@gmail.com>
2024-05-02 17:23:34 +0500

    Try Catch Block Added

--- source/navigation/BarberBoardNavigation/index.js
+++ source/navigation/BarberBoardNavigation/index.js
@@ -44,13 +44,22 @@ const BarberStack = () => {
   };
 
   const getCurrentLocation = async userAsyncDetails => {
-    const userCurrentLocation = await requestLocationPermissionAndGetLocation();
-    handleLocationChange(userAsyncDetails, userCurrentLocation);
+    try {
+      const userCurrentLocation =
+        await requestLocationPermissionAndGetLocation();
+      handleLocationChange(userAsyncDetails, userCurrentLocation);
+    } catch (error) {
+      console.log("getCurrentLocation",error);
+    }
   };
 
   const handleLocationChange = (userAsyncDetails, newCoords) => {
-    handleSaveBarberLocation(userAsyncDetails, newCoords?.coords);
-    setLogLatAsync(constants.AsyncStorageKeys.longLat, newCoords);
+    try {
+      handleSaveBarberLocation(userAsyncDetails, newCoords?.coords);
+      setLogLatAsync(constants.AsyncStorageKeys.longLat, newCoords);
+    } catch (error) {
+      console.log("handleLocationChange",error);
+    }
   };
 
   const handleSaveBarberLocation = (userAsyncDetails, newCoords) => {
