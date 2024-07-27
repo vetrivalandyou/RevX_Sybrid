cb7145a88f52c6b3a94284c633ec3dfeb5473b90
MuhammadAnas41 <muhdanassaleem@gmail.com>
2024-07-27 06:00:30 +0500

    LogBox Implemented

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
 import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-// LogBox.ignoreLogs(['Warning: ...']);
-
-// LogBox.ignoreAllLogs();
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...'])
+LogBox.ignoreAllLogs()
 
 AppRegistry.registerComponent(appName, () => App);
